736e9a34a4b24ba60a63ab87972f6316b2a77665
Matheus-AlvesCS <matheusalves.macs@gmail.com>
2026-06-10 08:57:16 -0300

    feat(refund): initialize form structure

--- src/pages/Refund.tsx
+++ src/pages/Refund.tsx
@@ -1,3 +1,18 @@
+import { Input } from "../components/Input"
+
 export function Refund() {
-  return <h1>Refund</h1>
+  return (
+    <form className="w-full flex flex-col gap-6">
+      <header>
+        <h1 className="text-gray-100 font-bold text-xl">
+          Solicitação de reembolso
+        </h1>
+        <p className="text-gray-200 text-sm mt-2 mb-4">
+          Dados da despesa para solicitar reembolso.{" "}
+        </p>
+      </header>
+
+      <Input required legend="Nome da solicitação" />
+    </form>
+  )
 }
