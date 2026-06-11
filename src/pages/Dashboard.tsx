cafb9e0f1885cdeac8e759d7b23e6975b2d7c18e
Matheus-AlvesCS <matheusalves.macs@gmail.com>
2026-06-11 10:03:24 -0300

    feat(dashboard): implement search input into dashboard

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,3 +1,29 @@
+import { useState } from "react"
+
+import { Input } from "../components/Input"
+
 export function Dashboard() {
-  return <h1>Solicitações</h1>
+  const [search, setSearch] = useState("")
+
+  function fetchUsers(e: React.SubmitEvent) {
+    e.preventDefault()
+
+    console.log(search)
+  }
+
+  return (
+    <div>
+      <h1 className="text-gray-100 text-xl font-bold">Solicitações</h1>
+
+      <form
+        onSubmit={fetchUsers}
+        className="flex items-center justify-between gap-3 mt-6 pb-6 border-b border-b-gray-400"
+      >
+        <Input
+          placeholder="Pesquisar pelo nome"
+          onChange={(e) => setSearch(e.target.value)}
+        />
+      </form>
+    </div>
+  )
 }
